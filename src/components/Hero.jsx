a51828ab2789eb734be4c25eceb173f625327520
Delimaci <gabriellec9@outlook.com>
2022-09-23 19:59:27 +0100

    changes to colours and nav

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,30 +7,30 @@ const Hero = () => {
     
 
 
-    <div className='w-full bg-[#121212]	py-10 md:py-8 px-4 justify-center '>
+    <div className='w-full  	py-10 md:py-8 px-4 justify-center '>
    <div className='max-w-[800px] mt-[-96px]  w-full h-screen mx-auto text-center flex flex-col justify-center text-white'>
   
         
-        <p className='rounded-full my-6 mx-auto py-2 bg-[#8a00e0] w-[230px] text-[#fff] font-bold p-2'>UI/UX Developer</p>
+        <p className='rounded-full my-6 mx-auto py-2 bg-[#ffa0f2]/90 w-[230px] text-[#fff] font-bold p-2'>UI/UX Developer</p>
         <div className="animate-wave text-5xl md:text-7xl ">👋</div>
         <br></br>
-        <h1 className='  px-10 md:text-7xl sm:text-6xl text-4xl font-bold md:py-6  '>Hi, I'm Delimaci. </h1>
+        <h1 className='  px-10 md:text-7xl sm:text-6xl text-4xl font-bold md:py-6  text-[#f5c0ff]'>Hi, I'm Delimaci. </h1>
 
        
         
         <div className='flex justify-center items-center'>
 
-        <p className='md:text-3xl sm:text-3xl text-md font-bold py-4'>Creating experiences through</p>
+        <p className='md:text-3xl sm:text-3xl text-md font-bold py-4 text-[#ffeafcd7]'>Creating experiences through</p>
 
         <Typed 
-        className='text-[#b640ff] md:text-4xl sm:text-2xl text-xl font-bold md:pl-4 pl-2 py-5'
+        className='text-[#ffa0f2] md:text-4xl sm:text-2xl text-xl font-bold md:pl-4 pl-2 py-5'
         strings={['HTML', 'CSS', 'REACTJS', 'Tailwind', 'Bootstrap']} 
         typeSpeed={120} 
         backSpeed={140} 
         loop
         />
         </div>
-        <p className='md:text-2xl text-xl font-bold text-gray-500 py-3'>and designing cool websites.</p>
+        <p className='md:text-2xl text-xl font-bold  text-[#ffd8f5] py-3'>and designing cool websites.</p>
 
 
 
@@ -40,12 +40,13 @@ const Hero = () => {
 
         <div className='p-4'>
 
-        <button className='bg-[#121212] text-[white]  border w-[200px] rounded-md shadow-md font-bold my-6 mx-auto py-5'>Say hi!</button>
+        <button className=' text-[#b7ff00] border-[#b7ff00] border w-[200px] rounded-md shadow-lg font-bold my-6 mx-auto py-5'>✉ Say hi!</button>
 
        
    
         </div>
         </div>
+        
    </div>
    
     
--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -2,6 +2,9 @@ import React, {useState} from 'react'
 import MenuItems from './MenuItems'
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai';
 import {FiTwitter} from 'react-icons/fi';
+import {FiFacebook} from 'react-icons/fi'
+import {AiOutlineGithub} from 'react-icons/ai'
+import {SiGithubsponsors} from 'react-icons/si'
 const Navbar2 = () => {
    
 
@@ -42,7 +45,7 @@ const Navbar2 = () => {
  
 
   return (
-    <div className={color ? 'shadow-xl transition-all duration-700 bg-white sticky top-0 z-50 ' : 'transition-all duration-700 top-0  '}>
+    <div className={color ? 'shadow-xl transition-all duration-700 bg-white sticky top-0 z-50 ' : 'transition-all duration-700 top-0 bg-transparent '}>
     <div className='z-50 sticky top-0  flex items-center justify-center h-24   mx-auto px-50 2xl:px-50  text-white'>
         <div className="text-2xl font-bold text-center uppercase ">
             <h1 className=' text-purple-400 text-4xl md:text-5xl  md:px-6'></h1>
@@ -66,10 +69,10 @@ const Navbar2 = () => {
         <FiTwitter  />
         </span>
         <span >
-        <FiTwitter />
+        <FiFacebook />
         </span>
         <span >
-        <FiTwitter />
+        <AiOutlineGithub />
         </span>
 </div>
         
